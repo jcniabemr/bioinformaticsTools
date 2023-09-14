254484e4787c1335e48d315e8f5bccde98be3beb
jcniabemr <john.connell@niab.com>
2023-09-14 16:21:20 +0100

    update

--- findMimpSequences.py
+++ findMimpSequences.py
@@ -15,16 +15,16 @@
 def writeGFF(sortedhits, filename):
     c = 0
     with open(os.path.splitext(filename)[0] + "_MIMP_hits.gff", 'w') as file:
-        file.write("\t".join(["Contig", "Tool", "MIMP_motif", "Start", "End", "Score", "Strand", "Phase", "INFO"]) + "\n")
+        file.write("\t".join(["Contig","Tool","MIMP_motif","Start","End","Score","Strand","Phase","INFO"]) + "\n")
         for x in sortedhits:
-            contig, start, stop, seq, strand = x.strip().split()
+            contig,start,stop,seq,strand = x.strip().split()
             c += 1
             file.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(contig, "findMimpSequences.py", seq, start, stop, ".", strand, ".", "ID=MIMP_" + str(c)))
 
 def mimpSearch(contig,sequence):
     foundHits = []
-    posHits = re.compile(r'CAGTGGG..GCAA[TA]AA').finditer(sequence)
-    negHits = re.compile(r'TT[TA]TTGC..CCCACTG').finditer(sequence)
+    posHits = re.compile(r'CAGT[GA][GA]G[GA][TG]GCAA.AAGT[TA]T.AAT.C[AC]').finditer(sequence)
+    negHits = re.compile(r'[GT]G.ATT.A[TA]ACTT.TTGC[CA][TC]C[TC][TC]ACTG').finditer(sequence)
     for x in posHits:
         foundHits.append("\t".join(map(str,[contig[1:],x.start(),x.end(),x.group(),"+"])))
     for x in negHits:
