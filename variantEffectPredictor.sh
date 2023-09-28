d94cb72fc9c21ed16351c6b0f4424437d4539310
jcniabemr <john.connell@niab.com>
2023-09-28 12:31:26 +0100

    update

--- variantEffectPredictor.sh
+++ variantEffectPredictor.sh
@@ -5,8 +5,7 @@
 #SBATCH --cpus-per-task=10
 
 ###################################################################################
-# Script to predict the effect of genetic variants and produce a gff              #
-#  of effected genes containing variants with predicted effect                    #
+# Script to predict the effect of genetic variants from a VCF                     #
 #                                                                                 #
 # Usage example:                                                                  #
 # python variantEffectorPredictor.sh --gff <.gff> --fasta <.fasta> --vcf <.vcf>   #
@@ -17,7 +16,43 @@
 # 2023                                                                            #
 ###################################################################################
 
-#vcfFile=$1 gffFile=$2 assembly=$3
+
+# ####Check and assign args 
+# while [[ $# -gt 0 ]]; do
+#   case ${1} in
+#     --vcf)
+#       vcfFile=${2}
+#       shift 
+#       shift 
+#       ;;
+#     --gff)
+#       gffFile=${2}
+#       shift 
+#       shift 
+#       ;;
+#     --fasta)
+#       fastaFile=${2}
+#       shift 
+#       shift 
+#       ;;
+#     *)
+#       echo "Unknown option: ${1}"
+#       exit 1
+#       ;;
+#   esac
+# done
+
+# if [ -z ${gffFile} ] || [ -z ${vcfFile} ] || [ -z ${fastaFile} ]; then
+# 	echo "Error: Please provide GFF, VCF, and FASTA files using --gff, --vcf, --fasta flags."
+# 	exit 1
+# else
+# 	echo "Using GFF file: ${gffFile}"
+# 	echo "Using VCF file: ${vcfFile}"
+# 	echo "Using FASTA file: ${fastaFile}"
+# fi
+
+
+
 
 cp ${1} ${TMPDIR}/VCF
 cd ${TMPDIR}
@@ -32,20 +67,25 @@ with open("VCF", 'r') as fi:
 		if strain not in variantDict:
 			variantDict[strain] = {}
 		for x in data[1:]:
-			if x[i][0] == "0":
-				variantDict[strain][x[1]] = x[3]
-			elif  x[i][0] == "1":
-				variantDict[strain][x[1]] = x[4]
+			if x[i][0] == "1" or x[i][2] == "1":
+				variantDict[strain]["_".join([x[0],x[1]])] = x[4]
+#			elif  x[i][0] == "0" and x[i][2] == "1":
+#				variantDict[strain]["_".join([x[0],x[1]])] = x[4]
 			else:
 				pass
 for x,y in variantDict.items():
-	print(x,y)
+	for y,z in y.items():
+		print(x,y,z)
 nestedVariants
 )
 
-
 while read -r x; do 
-	echo $x
+	strain=$(echo ${x} | awk '{print $1}')
+	contig=$(echo ${x} | awk '{split($2,a,"_");{print a[1]"_"a[2]}}')
+	position=$(echo ${x} | awk '{split($2,a,"_");{print a[3]}}')
+	allele=$(echo $x | awk '{print $3}')
+	#echo ${strain} ${contig} ${position} ${allele}
+
 done <<< ${alleles}
 
 
---
+++ vcf_to_fasta.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python 
+
+########################################################################
+# Script to produce fasta for each sample in a VCF file for use in     #
+# SNP based phylogeny                                                  #
+#                                                                      #
+# Usage example:                                                       #
+# python vcf_to_fasta.py --vcf <.vcf> --ploidy <1/2>                   #
+#                                                                      #
+# Written by John Connell                                              #
+# john.connell@niab.com                                                #
+# NIAB                                                                 #
+# 2023                                                                 #
+########################################################################
+
+def writeFile(res,vcf):
+	with open(os.path.splitext(vcf)[0] + ".fasta", 'w') as out:
+		for x in vcf:
+			out.write(f"{x}\n")
+
+def defineCodes():
+	iupac = {
+    'G/G': 'G', 'C/C': 'C', 'T/T': 'T', 'A/A': 'A',
+    'G/T': 'K', 'T/G': 'K', 'A/C': 'M', 'C/A': 'M',
+    'C/G': 'S', 'G/C': 'S', 'A/G': 'R', 'G/A': 'R',
+    'A/T': 'W', 'T/A': 'W', 'C/T': 'Y', 'T/C': 'Y',
+    './.': '.'
+}
+
+
+
+#def createFastaDipoid():
+
+
+def createFastaHaploid(vcf):
+	with open(vcf, 'r') as fi:
+		data=[x.strip().split() for x in fi.readlines() if not x.startswith("##")]
+		fastaFile = [] 
+		for i in range(9, len(data[0])):
+			fastaFile.append("".join([">",data[0][i]]))
+			seq = ""
+			for x in data[1:]:
+				if len(x[3]) > 1 or len(x[4]) > 1:
+					continue 
+				if x[i][0] == ".":
+					seq += "-"
+				elif x[i][0] == "0":
+					seq += x[3]
+				elif x[i][0] == "1":
+					seq += x[4]
+			fastaFile.append(seq)
+	writeFile(fastaFile,vcf)
+
+
+def main():
+	ap=argparse.ArgumentParser()
+	ap.add_argument(
+		'--vcf',
+		type = str,
+		required = True,
+		help = 'VCF file'
+		)
+	ap.add_argument(
+		'--ploidy',
+		type = str,
+		required = True,
+		help = 'Organism ploidy'
+		)
+	parse=ap.parse_args()
+	if parse.ploidy	== "1":
+		createFastaHaploid(parse.vcf)
+	else:
+		createFastaDiploid(parse.vcf)
+
+if __name__ == "__main__":
+	import argparse,os
+	main()
