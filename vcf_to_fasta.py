0fce457132d82095155e411167a296970819de0b
jcniabemr <john.connell@niab.com>
2023-09-29 11:16:57 +0100

    update

--- vcf_to_fasta.py
+++ vcf_to_fasta.py
@@ -15,22 +15,41 @@
 
 def writeFile(res,vcf):
 	with open(os.path.splitext(vcf)[0] + ".fasta", 'w') as out:
-		for x in vcf:
+		for x in res:
 			out.write(f"{x}\n")
 
-def defineCodes():
+def defineCodes(seq):
 	iupac = {
-    'G/G': 'G', 'C/C': 'C', 'T/T': 'T', 'A/A': 'A',
-    'G/T': 'K', 'T/G': 'K', 'A/C': 'M', 'C/A': 'M',
-    'C/G': 'S', 'G/C': 'S', 'A/G': 'R', 'G/A': 'R',
-    'A/T': 'W', 'T/A': 'W', 'C/T': 'Y', 'T/C': 'Y',
-    './.': '.'
-}
+	'G/G': 'G', 'C/C': 'C', 'T/T': 'T', 'A/A': 'A',
+	'G/T': 'K', 'T/G': 'K', 'A/C': 'M', 'C/A': 'M',
+	'C/G': 'S', 'G/C': 'S', 'A/G': 'R', 'G/A': 'R',
+	'A/T': 'W', 'T/A': 'W', 'C/T': 'Y', 'T/C': 'Y'
+	}
+	return iupac.get(seq,"-")
 
-
-
-#def createFastaDipoid():
-
+def createFastaDiploid(vcf):
+	with open(vcf, 'r') as fi:
+		data=[x.strip().split() for x in fi.readlines() if not x.startswith("##")]
+		fastaFile = []
+		for i in range(9, len(data[0])):
+			fastaFile.append("".join([">",data[0][i]]))
+			seq = ""
+			for x in data[1:]:
+				genotype = ""
+				if len(x[3]) > 1 or len(x[4]) > 1:
+					continue
+				ref = x[3]
+				alt = x[4]
+				if x[i][0] == ".":
+					seq += "-"
+				if x[i][0] == "0" and x[i][2] == "0":
+					seq += defineCodes(ref+"/"+ref)
+				if x[i][0] == "0" and x[i][2] == "1":
+					seq += defineCodes(ref+"/"+alt)
+				if x[i][0] == "1" and x[i][2] == "1":
+					seq += defineCodes(alt+"/"+alt)
+			fastaFile.append(seq)
+	writeFile(fastaFile,vcf)
 
 def createFastaHaploid(vcf):
 	with open(vcf, 'r') as fi:
@@ -50,7 +69,6 @@ def createFastaHaploid(vcf):
 					seq += x[4]
 			fastaFile.append(seq)
 	writeFile(fastaFile,vcf)
-
 
 def main():
 	ap=argparse.ArgumentParser()
