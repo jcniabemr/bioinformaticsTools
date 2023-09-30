765e76e8921121d8be275a20ca34d86f666529f3
jcniabemr <john.connell@niab.com>
2023-09-30 11:43:45 +0100

    update

--- variantEffectPredictor.sh
+++ variantEffectPredictor.sh
@@ -17,129 +17,94 @@
 ###################################################################################
 
 
-# ####Check and assign args 
-# while [[ $# -gt 0 ]]; do
-#   case ${1} in
-#     --vcf)
-#       vcfFile=${2}
-#       shift 
-#       shift 
-#       ;;
-#     --gff)
-#       gffFile=${2}
-#       shift 
-#       shift 
-#       ;;
-#     --fasta)
-#       fastaFile=${2}
-#       shift 
-#       shift 
-#       ;;
-#     *)
-#       echo "Unknown option: ${1}"
-#       exit 1
-#       ;;
-#   esac
-# done
+####Check and assign input args 
+while [[ $# -gt 0 ]]; do
+  case ${1} in
+    --vcf)
+      vcfFile=${2}
+      shift 
+      shift 
+      ;;
+    --gff)
+      gffFile=${2}
+      shift 
+      shift 
+      ;;
+    --fasta)
+      fastaFile=${2}
+      shift 
+      shift 
+      ;;
+    --ploidy)
+	  ploidy=${2}
+	  shift
+	  shift
+	  ;;
+    *)
+      echo "Unknown option: ${1}"
+      exit 1
+      ;;
+  esac
+done
 
-# if [ -z ${gffFile} ] || [ -z ${vcfFile} ] || [ -z ${fastaFile} ]; then
-# 	echo "Error: Please provide GFF, VCF, and FASTA files using --gff, --vcf, --fasta flags."
-# 	exit 1
-# else
-# 	echo "Using GFF file: ${gffFile}"
-# 	echo "Using VCF file: ${vcfFile}"
-# 	echo "Using FASTA file: ${fastaFile}"
-# fi
+if [ -z ${gffFile} ] || [ -z ${vcfFile} ] || [ -z ${fastaFile} ] || [ -z ${ploidy} ]; then
+	echo "Error: Please provide GFF, VCF, and FASTA files using --gff, --vcf, --fasta, --ploidy flags."
+	exit 1
+else
+	echo "Using GFF file: ${gffFile}"
+	echo "Using VCF file: ${vcfFile}"
+	echo "Using FASTA file: ${fastaFile}"
+	echo "Ploidy is ${ploidy}"
+fi
 
-
-
-
-cp ${1} ${TMPDIR}/VCF
+####Copy required infiles 
+cp ${vcfFile} ${TMPDIR}/VCF
+cp ${gffFile} ${TMPDIR}/GFF
+cp ${fastaFile} ${TMPDIR}/FASTA
 cd ${TMPDIR}
 
-alleles=$(
-python << nestedVariants
+####Funcion process diploid variants
+processDipVariants() { 
+python  << nestedVariants
 variantDict={}
-with open("VCF", 'r') as fi:
+with open("$1", 'r') as fi:
 	data = [x.strip().split() for x in fi.readlines() if not x.startswith("##")]
 	for i in range(9, len(data[0])):
 		strain = data[0][i]
 		if strain not in variantDict:
 			variantDict[strain] = {}
 		for x in data[1:]:
-			if x[i][0] == "1" or x[i][2] == "1":
-				variantDict[strain]["_".join([x[0],x[1]])] = x[4]
-#			elif  x[i][0] == "0" and x[i][2] == "1":
-#				variantDict[strain]["_".join([x[0],x[1]])] = x[4]
+			if x[i][0] == "1" and x[i][2] == "1":
+				variantDict[strain]["_".join(x[0:2] + x[3:5])] = [x[4] + "/" + x[4]]
+			elif  x[i][0] == "0" and x[i][2] == "1":
+				variantDict[strain]["_".join(x[0:2] + x[3:5])] = [x[3] + "/" + x[4]]
 			else:
 				pass
-for x,y in variantDict.items():
-	for y,z in y.items():
-		print(x,y,z)
+	for x,y in variantDict.items():
+		for y,z in y.items():
+			print(x,y,z)
 nestedVariants
-)
+}
+
+####Determine run type 
+if [ ${ploidy} -eq 1 ]; then
+	alleles=$(processHapVariants VCF)
+elif [ ${ploidy} -eq 2 ]; then 
+	alleles=$(processDipVariants VCF)
+else
+	echo "Ploidy ${ploidy} is not aloud, please set a ploidy of either 1 or 2"
+	exit 1
+fi
+
 
 while read -r x; do 
-	strain=$(echo ${x} | awk '{print $1}')
-	contig=$(echo ${x} | awk '{split($2,a,"_");{print a[1]"_"a[2]}}')
-	position=$(echo ${x} | awk '{split($2,a,"_");{print a[3]}}')
-	allele=$(echo $x | awk '{print $3}')
-	#echo ${strain} ${contig} ${position} ${allele}
-
+	echo $x
+	# strain=$(echo ${x} | awk '{print $1}')
+	# contig=$(echo ${x} | awk '{split($2,a,"_");{print a[1]"_"a[2]}}')
+	# position=$(echo ${x} | awk '{split($2,a,"_");{print a[3]}}')
+	# allele=$(echo $x | awk '{print $3}')
+	# echo ${strain} ${contig} ${position} ${allele}
 done <<< ${alleles}
 
 
 
-# #def writeGFF():
-
-
-# def processVariants(vcf):
-# 	header=[x for x in vcf if x.startswith("#")]
-# 	#for x in vcf:
-# 	#	samples = x[9:]
-
-
-
-
-
-# def readFiles(gff,fasta,vcf):
-# 	with open(gff, 'r') as fi:
-# 		gffData = [x.strip().split() for x in fi.readlines() if not x.startswith("#")]
-# 	with open(fasta, 'r') as fii:
-# 		genome = [x.strip() for x in fii.readlines() if not x.startswith("#")]
-# 	with open(vcf, 'r') as fiii:
-# 		vcfData = [x.strip().split() for x in fiii.readlines() if not x.startswith("##")]
-# 	return gffData,genome,vcfData
-
-# def main():
-# 	ap=argparse.ArgumentParser()
-# 	ap.add_argument(
-# 		'--gff',
-# 		required = True,
-# 		type = str,
-# 		help = 'gff file'
-# 		)
-# 	ap.add_argument(
-# 		'--fasta',
-# 		required = True,
-# 		type = str,
-# 		help = 'Reference genome'
-# 		)
-# 	ap.add_argument(
-# 		'--vcf',
-# 		required = True,
-# 		type = str,
-# 		help = 'Variant call file'
-# 		)
-# 	parse=ap.parse_args()
-# 	processVariants(
-# 	readFiles(
-# 	parse.gff,
-# 	parse.fasta,
-# 	parse.vcf
-# 	))
-
-
-# if __name__=="__main__":
-# 	import argparse
-# 	main()
