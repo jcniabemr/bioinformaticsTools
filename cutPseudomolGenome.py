921ac136d23cf4a08ad2eb80592cb0046b7a92d9
jcniabemr <john.connell@niab.com>
2024-06-28 10:11:23 +0100

    update

--- cutPseudomolGenome.py
+++ cutPseudomolGenome.py
@@ -3,7 +3,7 @@
 #######################################################################################
 # Script to cut a genome into fragments that contains "N" characters                  #
 #                                                                                     # 
-# Options: --assembly --contig --step --window                                        #
+# Options: --assembly --step --window                                                 #
 #                                                                                     #
 # Written by John Connell                                                             #
 # john.connell@niab.com                                                               #
@@ -11,67 +11,72 @@
 # 2024                                                                                #
 #######################################################################################
 
-def cutGenome(assembly,contig,step,window):
-    found = False
-    finished = False
-    seq = ""
-    lenCounter = 0 
-    with open(assembly, 'r') as file, open(os.path.splitext(assembly)[0] + "_" + contig + "_" + str(window)+ "_" + str(step) + ".fasta", 'w') as out:
-        for x in file:
-            x = x.strip()
-            if x.startswith(">"+contig):
-                found = True
-                continue 
-            if found:
-                for i in x:
-                    lenCounter += 1
-                    if lenCounter == start:
-                        seq += i.upper()
-                        if lenCounter == end:
-                            finished = True
-                            break
-            if finished:
-                break
-        for i in range(0, len(seq), step):
-            newEnd = min(i + window, len(seq))
-            chunk = seq[i:newEnd]
-            out.write(f">{assembly.split('.')[0].split('/')[-1]}_{contig}_{start + i-1}_{start + i + window-1}_{window}_{step}\n") 
-            out.write("\n".join([chunk[o:o + 60] for o in range(0, len(chunk), 60)]) + "\n")
+def cutGenome(assembly,step,window):
+	found = False 
+	written = False
+	header = ""
+	with open(assembly, 'r') as file, open(os.path.splitext(assembly)[0] + "_split_into_" + str(window)+ "_at_" + str(step) + "_steps.fasta", 'w') as out:
+		for x in file:
+			x = x.strip()
+			if x.startswith(">"):
+				if found:
+					for n in range(0, len(seq), step):
+						newEnd = min(n + window, len(seq))
+						chunk = seq[n:newEnd]
+						out.write(f"{header}_{n}_{newEnd}_{window}_{step}\n") 
+						out.write("\n".join([chunk[o:o + 60] for o in range(0, len(chunk), 60)]) + "\n")
+				found = True
+				seq = ""
+				header = x
+				continue
+			for i in x:
+				if i not in ['N', 'n']:
+					seq += i.upper()
+					written = False 
+				else:
+					if not written:
+						for n in range(0, len(seq), step):
+							newEnd = min(n + window, len(seq))
+							chunk = seq[n:newEnd]
+							out.write(f"{header}_{n}_{newEnd}_{window}_{step}\n") 
+							out.write("\n".join([chunk[o:o + 60] for o in range(0, len(chunk), 60)]) + "\n")
+						seq = ""
+						written = True
+					else:
+						continue 
+		for n in range(0, len(seq), step):
+			newEnd = min(n + window, len(seq))
+			chunk = seq[n:newEnd]
+			out.write(f"{header}_{n}_{newEnd}_{window}_{step}\n") 
+			out.write("\n".join([chunk[o:o + 60] for o in range(0, len(chunk), 60)]) + "\n")
 
 def main():
-    ap = argparse.ArgumentParser()
-    ap.add_argument(
-        '--assembly',
-        type = str,
-        required = True,
-        help = 'Assembly to be cut'
-    )
-    ap.add_argument(
-        '--step',
-        type = int,
-        required = True,
-        help = 'Step length for cutting'
-    )
-    ap.add_argument(
-        '--window',
-        type = int,
-        required = True,
-        help = 'Length of read'
-    )
-    ap.add_argument(
-        '--contig',
-        type = str,
-        required = True,
-        help = 'Contig for cutting'
-    )
-    parse=ap.parse_args()
-    cutGenome(
-        parse.assembly,
-        parse.contig,  
-        parse.step,
-        parse.window 
-    )
+	ap = argparse.ArgumentParser()
+	ap.add_argument(
+		'--assembly',
+		type = str,
+		required = True,
+		help = 'Assembly to be cut'
+	)
+	ap.add_argument(
+		'--step',
+		type = int,
+		required = True,
+		help = 'Step length for cutting'
+	)
+	ap.add_argument(
+		'--window',
+		type = int,
+		required = True,
+		help = 'Length of read'
+	)
+	parse=ap.parse_args()
+	cutGenome(
+		parse.assembly,
+		parse.step,
+		parse.window
+	)
 
 if __name__ == '__main__':
-    import os,argparse
-    main()
+	import os,argparse
+	main()
